a74e2c24828bf33e0f02af99835d938f6515ea46
SmythyCosta <smythy.costa@hotmail.com>
2019-08-11 10:57:14 -0300

    login e register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 
-    `<router-outlet></router-outlet>`,
+  templateUrl: './app.component.html',
 })
-export class AppComponent { }
+export class AppComponent {
+  title = 'app-tes';
+}
