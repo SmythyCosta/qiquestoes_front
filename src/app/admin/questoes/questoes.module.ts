6c17311edab0bcc7fa3fcbb6373f4669022c67d8
SmythyCosta <smythy.costa@hotmail.com>
2019-08-11 08:22:16 -0300

    disciplina r

--- src/app/admin/questoes/questoes.module.ts
+++ src/app/admin/questoes/questoes.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListarComponent } from './listar/listar.component';
 import { FormComponent } from './form/form.component';
+import { QuestoesRoutes } from './questoes.routing';
 
 
 
 @NgModule({
   declarations: [ListarComponent, FormComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    QuestoesRoutes
   ]
 })
 export class QuestoesModule { }
---
+++ src/app/admin/questoes/questoes.routing.ts
@@ -0,0 +1,25 @@
+// ############## Core Angular ##############
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// ############## Components ##############
+
+
+// ############## Helpers ##############
+
+
+
+const questoesRoutes: Routes = [
+    //{ path: '', component: ListarComponent },
+    // { path: 'success/:success', component: BlocoListComponent },
+    // path: 'form', component: FormComponent },
+    // { path: 'form/:id', component: BlocoFormComponent, canActivate: [CheckCondominioGuard] },
+    // { path: 'visualizar/:id', component: BlocoVisualizarComponent, canActivate: [CheckCondominioGuard] },
+    // { path: '404', component: Status404Component },
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(questoesRoutes)],
+    exports: [RouterModule]
+})
+export class QuestoesRoutes { }
