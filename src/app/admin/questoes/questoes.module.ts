3a5903ecd5fa844457422314f99980b96e78fa93
SmythyCosta <smythy.costa@hotmail.com>
2019-08-11 08:11:29 -0300

    questoes form

--- src/app/admin/questoes/questoes.module.ts
+++ src/app/admin/questoes/questoes.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListarComponent } from './listar/listar.component';
+import { FormComponent } from './form/form.component';
 
 
 
 @NgModule({
-  declarations: [ListarComponent],
+  declarations: [ListarComponent, FormComponent],
   imports: [
     CommonModule
   ]
