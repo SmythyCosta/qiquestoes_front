ecc61e35791d44432d2f482a59206c110d494b12
SmythyCosta <smythy.costa@hotmail.com>
2019-08-11 08:20:59 -0300

    disciplina r

--- src/app/admin/disciplina/disciplina.module.ts
+++ src/app/admin/disciplina/disciplina.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormComponent } from './form/form.component';
 import { ListarComponent } from './listar/listar.component';
+import { DisciplinaRoutes } from './disciplina.routing';
 
 
 @NgModule({
   declarations: [FormComponent, ListarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    DisciplinaRoutes
   ]
 })
 export class DisciplinaModule { }
