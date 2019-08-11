64690738db2ab852f0fbf92ec2e6b722c08653d1
SmythyCosta <smythy.costa@hotmail.com>
2019-08-11 08:10:05 -0300

    disciplina g listar

--- src/app/admin/disciplina/disciplina.module.ts
+++ src/app/admin/disciplina/disciplina.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormComponent } from './form/form.component';
+import { ListarComponent } from './listar/listar.component';
 
 
 
 @NgModule({
-  declarations: [FormComponent],
+  declarations: [FormComponent, ListarComponent],
   imports: [
     CommonModule
   ]
