5de31fde1fb338b80ec13c31e975d2a0bb055856
SmythyCosta <smythy.costa@hotmail.com>
2019-08-12 22:54:07 -0300

    arquivo config

---
+++ src/app/app.config.ts
@@ -0,0 +1,52 @@
+/*
+ * APP Config.
+ * 
+ *   --------------------------------------------
+ *   | @Autor: AS Soft                         |
+ *   | @Email: <smythy.costa@gmail.com>        |
+ *   --------------------------------------------
+ * 
+ * Este é o arquivo de configuracoes globais da aplicacao.
+ * Obs: qualquer mudanca vai impactar por toda a aplicacao.
+ * 
+ */
+
+export const appConfig = {
+
+    /*
+    |--------------------------------------------------------------------------
+    | Erros Backend
+    |--------------------------------------------------------------------------
+    | Exibe os erros do backend.
+    | Eceptions
+    */
+    exibir_erros_Backend: 1,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Mengens de operação
+    |--------------------------------------------------------------------------
+    | Mengans seram mostradas para informar o
+    | status da operações.
+    */
+    mensagem_sucesso: 'Operação realizada com sucesso',
+    mensagem_erro: 'Falha ao realizada esta Operação',
+    mensagem_erro_sisAdmin: 'Erro, consulte o administrador do sistema.',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Load
+    |--------------------------------------------------------------------------
+    | tempo milisegundo do load.
+    */
+    loading_time: 0,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Template
+    |--------------------------------------------------------------------------
+    | tempo milisegundo do load.
+    */
+    path_assets: 'assets/AdminLTE-3.0.0-beta.2/AdminLTE-3.0.0-beta.2',
+    
+};
